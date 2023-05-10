abe33fda0eec9f720d910475622f9b48bb23cea8
Yurii Chernilov <zavsek.o2@gmail.com>
2023-05-10 21:16:53 +0300

    mainProject

---
+++ src/com/gmail/zavsek_o2/ShuttleNumbers.java
@@ -0,0 +1,19 @@
+package com.gmail.zavsek_o2;
+
+public class ShuttleNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        int i = 1;
+        while (count < 100) {
+            String shuttleNumber = String.format("%d", i);
+            i++;
+            if (shuttleNumber.contains("4") || shuttleNumber.contains("9")) {
+                continue;
+            }
+            System.out.println("Шатл №: " + shuttleNumber);
+            count++;
+        }
+    }
+}
+
+
